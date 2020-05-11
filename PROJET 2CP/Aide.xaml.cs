8838d7b07a5161bc17a5d200a19899b751cb6282
AkliYalaoui <akliyalaoui@gmail.com>
2020-05-11 21:28:50 +0100

    added the help and about section

--- PROJET 2CP/Aide.xaml.cs
+++ PROJET 2CP/Aide.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,47 @@ namespace PROJET_2CP
         public Aide()
         {
             InitializeComponent();
+            CreerHelper();
+        }
+        void CreerHelper()
+        {
+            Expander ex = new Expander();
+            ex.Header = new TextBlock();
+            for(int i=1;i<=7;i++)
+            {
+                System.Data.SqlClient.SqlConnection connection = new SqlConnection($@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename ={System.IO.Directory.GetCurrentDirectory()}\Aide.mdf; Integrated Security = True");
+                SqlCommand cmd = new SqlCommand("select * from [Table] where Id='" + Convert.ToString(i) + "'", connection);
+                SqlDataReader dr;
+                try
+                {
+                    connection.Open();
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        if (MainWindow.langue == 1)
+                        {
+                           // ex.Text = dr["questionAr"].ToString();
+                            ex.Header= dr["repAr"].ToString();
+                        }
+                        if (MainWindow.langue == 0)
+                        {
+                            //ex.Text = dr["questionFr"].ToString();
+                            ex.Header = dr["repFr"].ToString();
+                        }
+
+                    }
+                    dr.Close();
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
