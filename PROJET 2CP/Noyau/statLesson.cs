d0116e85bb7e0963eb03ddd38540a3ed45e8a909
AkliYalaoui <akliyalaoui@gmail.com>
2020-05-15 22:21:30 +0100

    fuck you chelfi 3ttay

---
+++ PROJET 2CP/Noyau/statLesson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJET_2CP.Noyau
+{
+    class statLesson
+    {
+        public string Lesson { get; set; }
+        public bool IsGoodAnswer { get; set; }
+
+        public statLesson(string a, bool b)
+        {
+            Lesson = a;
+            IsGoodAnswer = b;
+        }
+    }
+}
